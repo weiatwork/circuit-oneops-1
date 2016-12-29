1ef29186372a851739fd032267fd07b25689aaa3
rvangar <rvangara@walmartlabs.com>
2016-12-28 16:40:28 -0800

    Replace REDIS bug fix

--- components/cookbooks/ring/recipes/replace.rb
+++ components/cookbooks/ring/recipes/replace.rb
@@ -1,2 +1,18 @@
+deps = node.workorder.payLoad.DependsOn
+
+db_type = nil
+isRedis = false
+deps.each do |dep|
+  class_name = dep['ciClassName'].split('.').last
+  db_type = dep['ciClassName'].split('.').last.downcase
+  if class_name == "Redisio"
+    isRedis = true
+    include_recipe "ring::replace_#{db_type}"
+  end
+end
+
+if !isRedis
+  include_recipe "ring::add"
+end
 
 include_recipe "ring::add"
