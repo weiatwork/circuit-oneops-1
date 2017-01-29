7ce54cee30e2f0364783a80bf3874358eb7372eb
Imaad Ghouri <ighouri@walmartlabs.com>
2017-01-29 13:35:05 -0800

    adding failure messages for volme and storage

--- components/cookbooks/storage/recipes/node_lookup.rb
+++ components/cookbooks/storage/recipes/node_lookup.rb
@@ -1,23 +1,7 @@
-# Copyright 2016, Walmart Stores, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 storage_cloud_name = node[:workorder][:cloud][:ciName]
 storagecloud = node[:workorder][:services][:storage][storage_cloud_name][:ciAttributes]
-if !storagecloud[:volumetypemap].nil? 
-  volumetype_map= JSON.parse(storagecloud[:volumetypemap])
-end
+volumetype_map= JSON.parse(storagecloud[:volumetypemap]) if !storagecloud[:volumetypemap].nil?
 
 if volumetype_map.count == 0
   node.set[:volume_type_from_map] = ""
@@ -30,8 +14,5 @@ Chef::Log.debug("node_lookup volume_type_selected: #{volume_type_selected}")
 if volumetype_map[volume_type_selected] != nil
    node.set[:volume_type_from_map] = volumetype_map[volume_type_selected]
 else
-   puts "***FAULT:FATAL=Volume Type #{volume_type_selected} Not found ***"
-   e = Exception.new("no backtrace")
-   e.set_backtrace("")
-   raise e
+	exit_with_error("Volume Type #{volume_type_selected} Not found")
 end
