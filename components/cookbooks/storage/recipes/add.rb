d61255e35d8c661d37b0de5d6ece97c0eaa741e2
Imaad Ghouri <ighouri@walmartlabs.com>
2017-01-31 14:04:37 -0800

    adding copyright section

--- components/cookbooks/storage/recipes/add.rb
+++ components/cookbooks/storage/recipes/add.rb
@@ -1,4 +1,29 @@
 
+# Copyright 2016, Walmart Stores, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# storage::add
+#
+# adds block storage ... volume component will attach and create raid device and filesystem
+#
+
+
+# TODO: fix fog/aws devices to remove the /dev/[s|vx]d\d+ limitation
+# only 15*8 devices available from sdi1-sdp15 (efgh used for ephemeral)
+#
+
 require File.expand_path('../../../azure_base/libraries/utils.rb', __FILE__)
 require 'json'
 include_recipe 'shared::set_provider'
