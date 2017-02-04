bb590e614ac75c60e61eca9df7adb2bd5bd93d1d
Imaad Ghouri <ighouri@walmartlabs.com>
2017-02-03 18:05:55 -0800

    adding failure messages on UI upon failure

--- components/cookbooks/database/libraries/util.rb
+++ components/cookbooks/database/libraries/util.rb
@@ -7,8 +7,8 @@ end
 def execute_command(command)
 	output = `#{command} 2>&1`
 	if $?.success?
-		Chef::Log.info("#{command} got successful.. #{output.gsub(/\n+/, ".")}")
+		Chef::Log.info("#{command} got successful.. #{output.gsub(/\n+/, '.')}")
 	else
-        exit_with_error "#{command} got failed.. #{output.gsub(/\n+/, ".")}"
+        exit_with_error "#{command} got failed.. #{output.gsub(/\n+/, '.')}"
 	end
 end
