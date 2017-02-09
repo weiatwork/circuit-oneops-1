0d71bd52af7e2ad67e258583a006e779048f4b4b
jayeshpise <jayesh.pise@gmail.com>
2017-02-08 16:13:55 -0800

    exiting with error in case cert cloud service is missing and auto-provision is checked

--- components/cookbooks/certificate/recipes/add.rb
+++ components/cookbooks/certificate/recipes/add.rb
@@ -2,16 +2,17 @@ require 'date'
 
 cloud_name = node[:workorder][:cloud][:ciName]
 provider = ""
+auto_provision = node.workorder.rfcCi.ciAttributes.auto_provision
 cert_service = node[:workorder][:services][:certificate]
+
 if ! cert_service.nil? && ! cert_service[cloud_name].nil?
 	provider = node[:workorder][:services][:certificate][cloud_name][:ciClassName].gsub("cloud.service.","").downcase.split(".").last
-else 
-	Chef::Log.info("Cert Service not defined for this cloud")
+elsif !auto_provision.nil? && auto_provision == "true"
+        Chef::Log.error("Certificate cloud service not defined for this cloud")
+        exit 1
 end
 
 expires_on = node[:expiry_time]
-
-auto_provision = node.workorder.rfcCi.ciAttributes.auto_provision
 
 if !auto_provision.nil? && auto_provision == "true" && !provider.nil? && !provider.empty?
 	include_recipe provider + "::add_certificate" 
