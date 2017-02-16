4ea2a28ca91d436d065609da790e42430dd388d7
Imaad Ghouri <ighouri@walmartlabs.com>
2017-02-16 11:57:52 -0800

    fixing volume issue

--- components/cookbooks/volume/recipes/add.rb
+++ components/cookbooks/volume/recipes/add.rb
@@ -739,7 +739,8 @@ ruby_block 'ramdisk tmpfs' do
     `mount | grep #{_mount_point}`
     if $?.to_i == 0
       Chef::Log.info("device #{_device} for mount-point #{_mount_point} already mounted.Will unmount it.")
-      execute_command("umount #{_mount_point}`")
+      `umount #{_mount_point}`
+      Chef::Log.error("umount error: #{umount_res.to_s}") if $?.to_i != 0
     end
 
     if _options == nil || _options.empty?
@@ -750,8 +751,8 @@ ruby_block 'ramdisk tmpfs' do
 
     # Make directory if not existing
     `mkdir -p #{_mount_point}`
-
-    execute_command("mount -t #{_fstype} -o size=#{size} #{_fstype} #{_mount_point}")
+    `mount -t #{_fstype} -o size=#{size} #{_fstype} #{_mount_point}"`
+    Chef::Log.error("mount error: #{result.to_s}") if $?.to_i != 0
 
     # clear existing mount_point and add to fstab again to ensure update attributes and to persist the ramdisk across reboots
     execute_command("grep -v #{_mount_point} /etc/fstab > /tmp/fstab")
