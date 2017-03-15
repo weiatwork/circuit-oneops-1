8f9a0d75e5f75383b02f691ad7e4248a8d56e3b9
Daniel Montroy <Daniel.Montroy@walmart.com>
2017-03-14 22:25:44 -0700

    Added V1 of Spark pack code

---
+++ components/cookbooks/spark-cassandra-v1/attributes/default.rb
@@ -0,0 +1,5 @@
+# default - Default attribute values for the pack
+
+default['spark-cassandra-vrc8']['spark_version'] = 'auto'
+default['spark-cassandra-vrc8']['connector_tarball'] = ''
+default['spark-cassandra-vrc8']['spark_base'] = '/opt'
