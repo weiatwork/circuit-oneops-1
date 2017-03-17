916ca3cd81b8a640e9448d0236e74a224087349d
Imaad Ghouri <ighouri@walmartlabs.com>
2017-03-17 15:27:57 -0700

    using excute resource

--- components/cookbooks/rabbitmq_cluster/recipes/join.rb
+++ components/cookbooks/rabbitmq_cluster/recipes/join.rb
@@ -1,10 +1,8 @@
 
 include_recipe "rabbitmq_cluster::app_stop" unless node.current_hostname == node.selected_hostname
 
-ruby_block "joining #{node.current_hostname} with #{node.selected_hostname}" do
-	block do
-		execute_command "rabbitmqctl join_cluster rabbit@#{node.selected_hostname}"
-	end
+execute "joining #{node.current_hostname} with #{node.selected_hostname}" do
+	command "rabbitmqctl join_cluster rabbit@#{node.selected_hostname}"
 	not_if { node.current_hostname == node.selected_hostname }
 end
 
