186767e6b6b7959b02b98de34fba1167db670034
James Wood <james.wood@walmart.com>
2017-03-21 13:14:33 -0500

    Removed 8.5.7 and 8.5.9 from available Tomcat versions

--- components/cookbooks/tomcat-2/metadata.rb
+++ components/cookbooks/tomcat-2/metadata.rb
@@ -18,7 +18,7 @@ attribute 'version',
           :format => {
               :important => true,
               :help => "Specify the version of Tomcat you wish to install.",
-              :form => {"field" => "select", "options_for_select" => [["8.5.8", "8.5.8"],["8.5.9", "8.5.9"],["8.5.12", "8.5.12"]]},
+              :form => {"field" => "select", "options_for_select" => [["8.5.12", "8.5.12"]]},
               :pattern => "[0-9\.]+",
               :category => "1.Global",
               :order => 1
