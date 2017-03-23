95d70c4988ff2d6eeadcf8deee8f7f9f19cd8e0f
Phat Huynh <sleepyorion@gmail.com>
2017-03-23 04:16:50 -0700

    Roll ansible-galaxy wrapper into ansible component
    - First attempt at using new format of URL for roles and playbook

--- components/cookbooks/ansible/recipes/add.rb
+++ components/cookbooks/ansible/recipes/add.rb
@@ -1,15 +1,20 @@
+require 'yaml'
+require 'tempfile'
+
 %w(python-devel openssl-devel git).each do |p|
 	package p do
 		action :install
 	end
 end
 
-if node.workorder.rfcCi.ciAttributes.pip_config == "true"
-  ruby_block "create pip directory" do
+roles = parse_url(node.workorder.rfcCi.ciAttributes.roles)
+playbook = parse_url(node.workorder.rfcCi.ciAttributes.playbook)
+
+unless node.workorder.rfcCi.ciAttributes.pip_proxy_config
+  ruby_block "create pip proxy content" do
     block do
       Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-      out = shell_out("mkdir -p ~/.pip")
-      conf = shell_out("echo \"#{node.workorder.rfcCi.ciAttributes.pip_config_content}\" > ~/.pip/pip.conf")
+      conf = shell_out("echo \"#{node.workorder.rfcCi.ciAttributes.pip_proxy_content}\" > /etc/pip.conf")
     end
   end
 end
@@ -46,3 +51,26 @@ template "/etc/ansible/hosts" do
 end
 
 puts "***RESULT:ansible_version=#{version}"
+
+# Let's install role
+roles.each do |role|
+  role_h = [{ 'src' => role[:url]}]
+  role_h[0]['name'] = role[:query]['name'] if role[:query].has_key? 'name'
+  role_h[0]['scm'] = role[:query]['scm'] if role[:query].has_key? 'scm'
+  role_h[0]['version'] = role[:query].has_key?('version') ? role[:query]['version'] : 'master'
+
+  puts "***** #{role_h.to_yaml}"
+
+  filename = Dir::Tmpname.make_tmpname "#{Chef::Config['file_cache_path']}/ansible_role", nil
+  # ansible-galaxy is quirky that it required extension to be .yml
+  filename = "#{filename}.yml"
+
+  file "#{filename}" do
+    content "#{role_h.to_yaml.sub("---","")}"
+  end
+
+  ansible_galaxy "#{filename}" do
+    action :install_file
+  end
+
+end
