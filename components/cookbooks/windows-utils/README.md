a38c47c25a463d58d42d04073795d4e6d013a227
Dennis Schelchkov <d0s00n9@homeoffice.wal-mart.com>
2017-05-22 09:53:12 -0700

    Adding accessory cookbook for windows deployments

---
+++ components/cookbooks/windows-utils/README.md
@@ -0,0 +1,4 @@
+# windowsos
+
+TODO: Enter the cookbook description here.
+
---
+++ components/cookbooks/windows-utils/metadata.rb
@@ -0,0 +1,6 @@
+name             "Windows-utils"
+description      "Accessory cookbook for Windows support in OneOps"
+version          "0.1.0"
+maintainer       "OneOps"
+maintainer_email "support@oneops.com"
+license          "Apache License, Version 2.0"
