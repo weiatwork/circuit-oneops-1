dbfb73d312f5179503b5289d76e2b8f988599155
Dennis Schelchkov <d0s00n9@homeoffice.wal-mart.com>
2017-05-22 15:33:41 -0700

    Fixing model sync

--- components/cookbooks/windows-utils/metadata.rb
+++ components/cookbooks/windows-utils/metadata.rb
@@ -4,3 +4,8 @@ version          "0.1.0"
 maintainer       "OneOps"
 maintainer_email "support@oneops.com"
 license          "Apache License, Version 2.0"
+
+grouping 'default',
+  :access   => 'global',
+  :packages => [ 'base', 'mgmt.catalog', 'mgmt.manifest', 'catalog', 'manifest', 'bom' ]
+  
