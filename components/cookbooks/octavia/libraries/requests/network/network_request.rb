4ae9ad8c0f02eadcba7ec1b6e49ea82203a04eae
Kowsalya Palaniappan - kpalan1 <kowsalya.palaniappan@walmart.com>
2017-06-06 17:15:18 -0500

    changes to support multiple provider network and remove neutron cookbook and service

--- components/cookbooks/octavia/libraries/requests/network/network_request.rb
+++ components/cookbooks/octavia/libraries/requests/network/network_request.rb
@@ -1,22 +1,23 @@
 require File.expand_path('../../base_request.rb', __FILE__)
 require 'excon'
 
-class SubnetRequest < BaseRequest
+class NetworkRequest < BaseRequest
 
-  def list_subnets(filters = {})
+  def list_networks(filters = {})
     request(
         :expects => 200,
         :method  => 'GET',
-        :path    => '/subnets',
+        :path    => '/networks',
         :query   => filters
     )
   end
 
-  def get_subnet(pool_id)
+  def get_ip_availability(filters = {})
     request(
         :expects => 200,
         :method  => 'GET',
-        :path    => "/subnets/#{pool_id}"
+        :path    => '/network-ip-availabilities',
+        :query   => filters
     )
   end
 
