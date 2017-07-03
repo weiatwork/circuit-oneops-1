dfc2eb9ccf6b60486de11c12a8467e5c011f9722
Jayesh Pise <jayesh.pise@gmail.com>
2017-07-03 11:37:43 -0700

    (Don't merge yet) New component for Keywhiz-client and other related changes (#901)
    * Add Keywhiz client component and configure and start it
    
    * Add owner_email attribute on certificate component
    
    * Changes/Cleanup as per review comments

--- packs/base.rb
+++ packs/base.rb
@@ -751,7 +751,12 @@ resource "firewall",
      }'
    }
  }
- 
+
+resource "keywhiz-client",
+   :cookbook => "oneops.1.keywhiz-client",
+   :design => true,
+   :requires => {"constraint" => "0..1", 'services' => '*certificate,*keywhiz'}
+
 resource "artifact",
   :cookbook => "oneops.1.artifact",
   :design => true,
@@ -790,6 +795,9 @@ end
   { :from => 'sensuclient', :to => 'compute'  },
   { :from => 'library',     :to => 'os' },
   { :from => 'objectstore',  :to => 'compute'},
+  { :from => 'keywhiz-client',  :to => 'os'},
+  { :from => 'keywhiz-client',  :to => 'user'},  
+  { :from => 'keywhiz-client',  :to => 'certificate'},
   { :from => 'objectstore',  :to => 'user'}
 ].each do |link|
   relation "#{link[:from]}::depends_on::#{link[:to]}",
@@ -837,7 +845,7 @@ end
 
 # managed_via
 [ 'os', 'telegraf', 'user', 'job', 'file', 'volume', 'share', 'download', 'library', 'daemon', 
-  'certificate', 'logstash', 'sensuclient', 'artifact', 'objectstore'].each do |from|
+  'certificate', 'logstash', 'sensuclient', 'artifact', 'objectstore', 'keywhiz-client'].each do |from|
   relation "#{from}::managed_via::compute",
     :except => [ '_default' ],
     :relation_name => 'ManagedVia',
