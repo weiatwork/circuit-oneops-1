411d88a58f251f38beb602729a9f1a62986a9d6a
Shantanu Kande - S0K00II <s0k00ii@m-C02SP0XAG8WL.local>
2017-07-04 09:30:01 -0500

    will help user to resize volume on ephemeral and persistent storage

--- components/cookbooks/volume/recipes/replace.rb
+++ components/cookbooks/volume/recipes/replace.rb
@@ -1,1 +1,12 @@
-include_recipe "volume::add"
+new_size = node.workorder.rfcCi.ciAttributes["size"].gsub(/\s+/, "")
+old_size = node.workorder.rfcCi.ciBaseAttributes[:size]
+
+#this condition will help to attach same volume component on compute replace
+if !old_size.nil? && new_size != old_size &&  old_size != "-1"
+  Chef::Log.info("Volume will be deleted and new volume will be attached to compute")
+  include_recipe "volume::delete"
+  include_recipe "volume::add"
+else
+  Chef::Log.info("Same volume will be attached to compute")
+  include_recipe "volume::add"
+end
