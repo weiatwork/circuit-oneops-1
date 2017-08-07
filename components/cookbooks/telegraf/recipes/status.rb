394509816b2512ed2ef73f676322e6f7d59b4418
rrupaku <rrupaku@wal-mart.com>
2017-08-07 11:38:01 +0530

    telegraf component changes to include support for windows

--- components/cookbooks/telegraf/recipes/status.rb
+++ components/cookbooks/telegraf/recipes/status.rb
@@ -1,17 +1,30 @@
 
 #name = node['telegraf']['name']
-name = node.workorder.payLoad.RealizedAs[0].ciName
-initd_filename = 'telegraf'
-if(name.empty? || name.nil?)
-  Chef::Log.info("instance name is not set. use default.")
-else
-  initd_filename = initd_filename + "_" + name
-end
 
-ruby_block "check telegraf status" do
-    block do
-      Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-      shell_out!("service #{initd_filename} status",
-        :live_stream => Chef::Log::logger)
+case node.platform
+when "windows"
+    ruby_block "Validate_service" do
+      block do
+        ::Chef::Recipe.send(:include, Chef::Mixin::PowershellOut)
+        ps_code = "return get-service -name 'telegraf' -ErrorAction 'silentlycontinue' | %{$_.Status}"
+        cmd = powershell_out(ps_code)
+        Chef::Log.info("Status of telegraf service is - #{cmd.stdout}")
+      end
     end
+else
+  name = node.workorder.payLoad.RealizedAs[0].ciName
+  initd_filename = 'telegraf'
+  if(name.empty? || name.nil?)
+    Chef::Log.info("instance name is not set. use default.")
+  else
+    initd_filename = initd_filename + "_" + name
+  end
+
+  ruby_block "check telegraf status" do
+      block do
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+        shell_out!("service #{initd_filename} status",
+          :live_stream => Chef::Log::logger)
+      end
+  end
 end
