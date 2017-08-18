fe6868c497cd14c8d16668d88af6987248b87e64
Jayesh Pise <jayesh.pise@gmail.com>
2017-08-18 05:58:24 -0700

    lb-cert autogen related change to use ciBaseAttributes in case of add-rfc (#953)

--- components/cookbooks/netscaler/recipes/upload_cert_key.rb
+++ components/cookbooks/netscaler/recipes/upload_cert_key.rb
@@ -18,24 +18,33 @@ cloud_name = node[:workorder][:cloud][:ciName]
 cloud_service = node[:workorder][:services][:lb][cloud_name][:ciAttributes]
 Chef::Log.info("upload netscaler: #{node.netscaler_host}")
 
+certAttributes = ci[:ciAttributes]
+
+if !certAttributes[:auto_provision].nil? &&
+   certAttributes[:auto_provision] == "true" &&
+   !ci[:rfcAction].nil? && ci[:rfcAction] == "add"
+	Chef::Log.info("Cert is an add rfc and auto-generated")
+	certAttributes = ci[:ciBaseAttributes]
+end
+
 # write cert content to tmp file
 cert_file = "oo-cert-"+ci[:ciId].to_s+".cer"
 tmp_cert_file = "/tmp/"+cert_file
 ns_cert_file = "/nsconfig/ssl/"+cert_file
 node.set["ns_cert_file"] = cert_file
 
-::File.open(tmp_cert_file, 'w') { |file| file.write(ci[:ciAttributes][:cert].gsub("\C-M","")) }
+::File.open(tmp_cert_file, 'w') { |file| file.write(certAttributes[:cert].gsub("\C-M","")) }
 
 # write ca cert content to tmp file
-if ci[:ciAttributes].has_key?("cacertkey") &&
-   !ci[:ciAttributes][:cacertkey].empty?
+if certAttributes.has_key?("cacertkey") &&
+   !certAttributes[:cacertkey].empty?
   ca_cert_file = "oo-ca-cert-"+ci[:ciId].to_s+".cer"
   tmp_ca_cert_file = "/tmp/"+ca_cert_file
   ns_ca_cert_file = "/nsconfig/ssl/"+ca_cert_file
   node.set["ns_ca_cert_file"] = ca_cert_file
   node.set["ns_ca_cert_name"] = ca_cert_file.gsub(".cer","")
   
-  ::File.open(tmp_ca_cert_file, 'w') { |file| file.write(ci[:ciAttributes][:cacertkey].gsub("\C-M","")) }
+  ::File.open(tmp_ca_cert_file, 'w') { |file| file.write(certAttributes[:cacertkey].gsub("\C-M","")) }
 end
 
 # write key content to tmp file
@@ -44,7 +53,7 @@ tmp_key_file = "/tmp/"+key_file
 ns_key_file = "/nsconfig/ssl/"+key_file
 node.set["ns_key_file"] = key_file
 
-::File.open(tmp_key_file, 'w') { |file| file.write(ci[:ciAttributes][:key].gsub("\C-M","")) }
+::File.open(tmp_key_file, 'w') { |file| file.write(certAttributes[:key].gsub("\C-M","")) }
 
 # verify using openssl
 cmd = "openssl rsa -in #{tmp_key_file} -passin pass:'#{node.cert[:passphrase]}'"
@@ -89,3 +98,4 @@ end
 if !ns_ca_cert_file.nil?
   ::File.delete(tmp_ca_cert_file)
 end
+
