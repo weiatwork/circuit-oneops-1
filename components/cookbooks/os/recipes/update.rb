a86915c7856066bdd5442b1328c2ced92ec4b787
Shantanu Kande <shaansaheb@gmail.com>
2017-11-23 13:26:45 -0600

    instance size and os type change requires replace

--- components/cookbooks/os/recipes/update.rb
+++ components/cookbooks/os/recipes/update.rb
@@ -2,4 +2,9 @@
 # Cookbook Name:: os
 # Recipe:: update
 #
-include_recipe "os::add"
+if (node[:workorder][:rfcCi][:ciBaseAttributes].has_key?("ostype") &&
+    (node[:workorder][:rfcCi][:ciBaseAttributes][:ostype] != node[:workorder][:rfcCi][:ciAttributes][:ostype]))
+  exit_with_error "OS type doens't match with current configuration, consider replacing compute or change OS type to original"
+else
+  include_recipe "os::add"
+end
