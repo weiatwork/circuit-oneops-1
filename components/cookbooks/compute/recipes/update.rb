00829863ffc7d7c80fc2bfea6c719ba63d127de3
Shantanu Kande <shaansaheb@gmail.com>
2017-11-23 16:34:22 -0600

    change call to exit with error

--- components/cookbooks/compute/recipes/update.rb
+++ components/cookbooks/compute/recipes/update.rb
@@ -4,7 +4,7 @@
 #
 if (node[:workorder][:rfcCi][:ciBaseAttributes].has_key?("size")  &&
     (node[:workorder][:rfcCi][:ciBaseAttributes][:size] != node[:workorder][:rfcCi][:ciAttributes][:size]))
-  OOLog.fatal("Instance size doens't match with current configuration, consider replacing compute or change instace size to original")
+  exit_with_error "Instance size doens't match with current configuration, consider replacing compute or change instace size to original"
 else
   include_recipe "compute::add"
 end
