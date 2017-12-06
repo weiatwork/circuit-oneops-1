d1e2eb717d58084cea9632a709f5d3d712155229
rameshjanjyam <rameshjanjyam@live.com>
2017-12-06 16:51:48 -0600

    fixed bug in retrieving security group when more than one element is present in DependsOn attribute in workorder

--- components/cookbooks/azure/libraries/virtual_machine_manager.rb
+++ components/cookbooks/azure/libraries/virtual_machine_manager.rb
@@ -24,7 +24,7 @@ module AzureCompute
       @location = @compute_service[:location]
       @initial_user = @compute_service[:initial_user]
       @express_route_enabled = @compute_service['express_route_enabled']
-      @secgroup_name = node['workorder']['payLoad']['DependsOn'][0]['ciName']
+      @secgroup_name = get_security_group_name(node)
       @image_id = node['image_id'].split(':')
       @size_id = node['size_id']
       @ip_type = node['ip_type']
@@ -209,5 +209,16 @@ module AzureCompute
       end
       return storage_account, vhd_uri, datadisk_uri
     end
+
+    def get_security_group_name(node)
+      secgroup = node['workorder']['payLoad']['DependsOn'].detect { |d| d['ciClassName'] =~ /Secgroup/ }
+      if secgroup.nil?
+        OOLog.fatal("No Secgroup found in workorder. This is required for VM creation.")
+      else
+        return secgroup['ciName']
+      end
+    end
+
+    private :get_security_group_name
   end
 end
