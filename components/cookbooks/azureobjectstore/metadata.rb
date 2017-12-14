a704ed494c6826d3ea3a71978c1b081e756939e3
muhammad-asad-confiz <muhammad.asad@confiz.com>
2017-12-14 15:23:56 +0500

    AzureObjectstore metadata.rb update

--- components/cookbooks/azureobjectstore/metadata.rb
+++ components/cookbooks/azureobjectstore/metadata.rb
@@ -5,6 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures azure objectstore service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'azure'
 grouping 'default',
          :access => 'global',
          :packages => %w[base service.filestore mgmt.cloud.service cloud.service cloud.zone.service],
