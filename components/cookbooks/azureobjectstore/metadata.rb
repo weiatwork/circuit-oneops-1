f0be1148331d16f0ab8c5c34fa5937a1574e9fe8
muhammad-asad-confiz <muhammad.asad@confiz.com>
2017-12-15 17:23:44 +0500

    Changes made as per client request and improvements are done
    Summary:
    Merge branch 'master' of https://github.com/oneops/circuit-oneops-1 into azure-objectstore
    
    AzureObjectstore metadata.rb update
    
    Merge branch 'master' of https://github.com/oneops/circuit-oneops-1 into azure-objectstore
    
    ObjectStore improvements and changes
    
    Test Plan: None
    
    Subscribers: omair.sajid
    
    Differential Revision: http://phabricator.confiz.com/D11237

--- components/cookbooks/azureobjectstore/metadata.rb
+++ components/cookbooks/azureobjectstore/metadata.rb
@@ -5,52 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures azure objectstore service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'azure'
 grouping 'default',
          :access => 'global',
          :packages => %w[base service.filestore mgmt.cloud.service cloud.service cloud.zone.service],
          :namespace => true
-
-attribute 'tenant_id',
-          :description => 'Tenant ID',
-          :required => 'required',
-          :default => '',
-          :format => {
-            :help => 'tenant id',
-            :category => '2.Authentication',
-            :order => 1
-          }
-
-attribute 'client_id',
-          :description => 'Client ID',
-          :required => 'required',
-          :default => '',
-          :format => {
-            :help => 'client id',
-            :category => '2.Authentication',
-            :order => 2
-          }
-
-attribute 'subscription',
-          :description => 'Subscription ID',
-          :required => 'required',
-          :default => '',
-          :format => {
-            :help => 'subscription id in azure',
-            :category => '2.Authentication',
-            :order => 3
-          }
-
-attribute 'client_secret',
-          :description => 'Client Secret',
-          :required => 'required',
-          :encrypted => true,
-          :default => '',
-          :format => {
-            :help => 'client secret azure',
-            :category => '2.Authentication',
-            :order => 4
-          }
 
 attribute 'storage_account_name',
           :description => 'Storage Account Name',
@@ -58,7 +16,16 @@ attribute 'storage_account_name',
           :format => {
             :help => 'Storage Account Name',
             :category => '2.Authentication',
-            :order => 5
+            :order => 1
           }
 
-recipe 'azure_subscription_status', 'Check Azure Subscription Status'
+attribute 'storage_account_access_key',
+          :description => 'Storage Account Access Key',
+          :required => 'required',
+          :encrypted => true,
+          :default => '',
+          :format => {
+            :help => 'Storage Account Access Key',
+            :category => '2.Authentication',
+            :order => 2
+          }
--- components/cookbooks/objectstore/recipes/add.rb
+++ components/cookbooks/objectstore/recipes/add.rb
@@ -5,32 +5,19 @@ ciAttr = node[:workorder][:services][:filestore][cloud_name][:ciAttributes]
 case cloud_type
 when /swift/
   creds = {
-      :provider           => 'OpenStack',
-      :openstack_api_key  => ciAttr[:password],
-      :openstack_username => ciAttr[:username],
-      :openstack_tenant   => ciAttr[:tenant],
-      :openstack_auth_url => ciAttr[:endpoint] + '/tokens',
-      :openstack_region   => ciAttr[:regionname]
+    :provider           => 'OpenStack',
+    :openstack_api_key  => ciAttr[:password],
+    :openstack_username => ciAttr[:username],
+    :openstack_tenant   => ciAttr[:tenant],
+    :openstack_auth_url => ciAttr[:endpoint] + '/tokens',
+    :openstack_region   => ciAttr[:regionname]
   }
-when /azure/
-  require 'fog/azurerm'
+when /azureobjectstore/
   creds = {
-      :tenant_id       => ciAttr[:tenant_id],
-      :client_id       => ciAttr[:client_id],
-      :client_secret   => ciAttr[:client_secret],
-      :subscription_id => ciAttr[:subscription]
+    :provider                   => 'Azure',
+    :azure_storage_account_name => ciAttr[:storage_account_name],
+    :azure_storage_access_key   => ciAttr[:storage_account_access_key]
   }
-  conn = Fog::Storage::AzureRM.new(creds)
-  access_keys = nil
-  storage_account_name = ciAttr[:storage_account_name]
-  conn.storage_accounts.each do |storage_acc|
-    if storage_acc.name.eql?(storage_account_name)
-      access_keys = storage_acc.get_access_keys
-    end
-  end
-  creds[:azure_storage_access_key] = access_keys[1].value unless access_keys.nil?
-  creds[:azure_storage_account_name] = storage_account_name
-  creds[:provider] = 'Azure'
 end
 
 require 'json'
