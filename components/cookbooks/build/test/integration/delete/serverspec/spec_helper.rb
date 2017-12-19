4b5cefe65d89a5fb8fc8214ee86ca6eaeeba9c8e
shantanukande <shaansaheb@gmail.com>
2017-12-19 01:42:04 -0600

    kitchen-ci for build component (#1201)

--- components/cookbooks/build/test/integration/delete/serverspec/spec_helper.rb
+++ components/cookbooks/build/test/integration/delete/serverspec/spec_helper.rb
@@ -9,7 +9,11 @@ if ENV['OS'] == 'Windows_NT'
   $node = ::JSON.parse(File.read('c:\windows\temp\serverspec\node.json'))
 else
   set :backend, :exec
-  $node = ::JSON.parse(File.read('/tmp/serverspec/node.json'))
+  if !ENV['WORKORDER'].empty?
+    $node = ::JSON.parse(File.read(ENV['WORKORDER'].to_s))
+  else
+    $node = ::JSON.parse(File.read('/tmp/serverspec/node.json'))
+  end
 end
 
 set :path, '/sbin:/usr/local/sbin:/usr/sbin:$PATH' unless os[:family] == 'windows'
--- components/cookbooks/build/test/integration/deleteBuild/serverspec/delete_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command('ls /opt/build/current') do
-  its(:stderr) { should match /No such file or directory/ }
-end
