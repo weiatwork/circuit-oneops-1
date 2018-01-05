98f370b2c90c32a650013340252ac1313be620e0
Muhammad Asad <muhammad.asad@confiz.com>
2018-01-05 23:57:32 +0500

    Objectstore Improvements (#1245)

--- components/cookbooks/objectstore/test/integration/add/serverspec/add_spec.rb
+++ components/cookbooks/objectstore/test/integration/add/serverspec/add_spec.rb
@@ -5,9 +5,15 @@ require "#{$circuit_path}/circuit-oneops-1/components/spec_helper.rb"
 OBJECTSTORE_EXE_FILE   = '/usr/local/bin/objectstore'.freeze
 OBJECTSTORE_CREDS_FILE = '/etc/objectstore_creds.json'.freeze
 
-TEST_CONTAINER_NAME    = "test-#{$node['workorder']['rfcCi']['rfcId'].to_s}".freeze
+TEST_CONTAINER_NAME    = "test-#{$node['workorder']['rfcCi']['rfcId']}".freeze
 TEST_BLOB_NAME         = 'test-blob'.freeze
 TEST_BLOB_SIZE         = '10M'.freeze
+
+OUTPUT                 = "usage: \n"                                            \
+                         "objectstore list <container>\n"                       \
+                         "objectstore upload <directory/file> <container>\n"    \
+                         "objectstore download <container/blob> <local-path>\n" \
+                         "objectstore delete <container>/<blob>\n".freeze
 
 describe file(OBJECTSTORE_EXE_FILE) do
   it { should exist }
@@ -19,6 +25,14 @@ end
 
 describe file(OBJECTSTORE_CREDS_FILE) do
   it { should exist }
+end
+
+describe command(OBJECTSTORE_EXE_FILE) do
+  its(:stdout) { should eq OUTPUT }
+end
+
+describe command("#{OBJECTSTORE_EXE_FILE} --help") do
+  its(:exit_status) { should eq 0 }
 end
 
 describe command("fallocate -l #{TEST_BLOB_SIZE} #{TEST_BLOB_NAME}") do
