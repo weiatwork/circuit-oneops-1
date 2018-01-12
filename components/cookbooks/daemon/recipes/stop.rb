cb116eeda83f755d2f46f1c5893bb96860467dfd
Shantanu Kande <shaansaheb@gmail.com>
2018-01-12 17:34:45 -0600

    removed provider for service

--- components/cookbooks/daemon/recipes/stop.rb
+++ components/cookbooks/daemon/recipes/stop.rb
@@ -5,6 +5,15 @@ attrs = node.workorder.ci.ciAttributes
 service_name = attrs[:service_name]
 pat = attrs[:pattern] || ''
 
+service_type = nil
+initService = `ls /ect/init.d/#{service_name}`
+systemdService = `ls /usr/lib/systemd/system/#{service_name}.service`
+if systemdService.include?("/usr/lib/systemd/system/#{service_name}.service")
+	service_type = "systemd"
+elsif initService.include?("/etc/init.d/#{service_name}")
+	service_type = "init"
+end
+
 # stop daemon service when pattern has not been specified
 ruby_block "stop #{service_name} service" do
 	block do
@@ -16,7 +25,7 @@ end
 
 # stop daemon service when pattern has been specified
 service "#{service_name}" do
-	provider Chef::Provider::Service::Init::Redhat if node[:platform_family].include?("rhel")
+	provider Chef::Provider::Service::Init if service_type == "init" && node[:platform_family].include?("rhel")
 	pattern "#{pat}"
 	action :stop
 	only_if { !pat.empty? }
