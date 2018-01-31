518269abcbc8ec8eed9cca960de603857ad0e0da
Sanjay Bonde <sbonde@walmartlabs.com>
2018-01-31 11:26:05 -0800

    removed design attr from lb

--- packs/solrcloud.rb
+++ packs/solrcloud.rb
@@ -32,7 +32,6 @@ variable "solr_jmx_port",
 resource "lb",
   :except => [ 'single' ],
   :cookbook => "oneops.1.lb",
-  :design => true,
   :attributes => {
     "listeners" => "[\"tcp 8983 tcp 8983\"]"
   }
