b64877232bb364f09b49e84e1d0354c59cec1698
Vikram Purohit <33705619+vsrp1010@users.noreply.github.com>
2018-02-14 15:14:12 -0800

    Updated service-mesh component to support latest version of soa-linkerd (#1324)
    Updated service-mesh component to use latest linkerd config template and use latest soa-linkerd artifact compatible with this template

---
+++ components/cookbooks/service-mesh/test/integration/update/serverspec/update_spec.rb
@@ -0,0 +1,3 @@
+is_windows = ENV['OS'] == 'Windows_NT'
+$circuit_path = "#{is_windows ? 'C:/Cygwin64' : ''}/home/oneops"
+require "#{$circuit_path}/circuit-oneops-1/components/cookbooks/service-mesh/test/integration/mesh_spec.rb"
