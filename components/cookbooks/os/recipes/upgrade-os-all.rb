301e7de7c3076cf2968461871347e8767b503e31
mtunganati <mtunganati@users.noreply.github.com>
2018-03-03 11:49:57 -0800

    Yum Update fix (#1374)
    * Adding logic to restart dhcp on repair
    
    * Removing save config
    
    * yum update fix

--- components/cookbooks/os/recipes/upgrade-os-all.rb
+++ components/cookbooks/os/recipes/upgrade-os-all.rb
@@ -7,7 +7,7 @@ if node.platform? "ubuntu"
   upgrade_cmd = "sudo apt-get -o Dpkg::Options::='--force-confnew' --force-yes -fuy dist-upgrade"
   cmd = "apt-get -y update; DEBIAN_FRONTEND=noninteractive #{upgrade_cmd}"  
 else
-  cmd = "yum -y update"    
+  cmd = "yum -y update --exclude=centos-release.x86_64"    
 end  
 
 execute cmd
