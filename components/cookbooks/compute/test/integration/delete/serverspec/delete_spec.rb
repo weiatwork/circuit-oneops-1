d864251d84495e9b95ccae9754a6973541329b1d
Cody White <cody.white0@walmart.com>
2018-03-13 08:56:33 -0500

    Refactored kitchen tests to conform to standards

--- components/cookbooks/compute/test/integration/delete/serverspec/delete_spec.rb
+++ components/cookbooks/compute/test/integration/delete/serverspec/delete_spec.rb
@@ -1,13 +1,19 @@
-CIRCUIT_PATH="/opt/oneops/inductor/circuit-oneops-1"
-COOKBOOKS_PATH="#{CIRCUIT_PATH}/components/cookbooks"
-AZURE_TESTS_PATH="#{COOKBOOKS_PATH}/azure/test/integration/delete/serverspec/tests"
+CIRCUIT_PATH     = '/opt/oneops/inductor/circuit-oneops-1'.freeze
+COOKBOOKS_PATH   = "#{CIRCUIT_PATH}/components/cookbooks".freeze
+AZURE_TESTS_PATH = "#{COOKBOOKS_PATH}/azure/test/integration/delete/serverspec/tests".freeze
 
 require "#{CIRCUIT_PATH}/components/spec_helper.rb"
 require "#{COOKBOOKS_PATH}/azure_base/test/integration/spec_utils"
 
 provider = SpecUtils.new($node).get_provider
-if provider =~ /azure/
-  require "#{COOKBOOKS_PATH}/azure_base/test/integration/azure_spec_utils"
-  Dir.glob("#{AZURE_TESTS_PATH}/*.rb").each {|tst| require tst}
+
+case provider
+  when /azure/
+    require "#{COOKBOOKS_PATH}/azure_base/test/integration/azure_spec_utils"
+    Dir.glob("#{AZURE_TESTS_PATH}/*.rb").each {|tst| require tst}
+  when /openstack/
+    require "#{COOKBOOKS_PATH}/compute/test/integration/delete/serverspec/openstack.rb"
+  else
+    puts "No tests for provider: #{provider}"
 end
 
---
+++ components/cookbooks/compute/test/unit/spec/compute_util_spec.rb
@@ -0,0 +1,73 @@
+# Unit tests for circuit-oneops-1/components/cookbooks/compute/libraries/compute_until.rb
+
+
+require_relative '../../../libraries/compute_util'
+require_relative 'image'
+
+describe 'get_image' do
+  # Set up data set
+  images = Array.new
+  File.foreach('./spec/list_of_images.txt') do |line|
+    image = line.split(',')
+    images << Image.new(image[1].gsub("\n", ''),image[0])
+  end
+
+  # Base unchanging vars
+  default_image = Image.new('CentOS-7.3.1611-x86_64-minimal-cloud-init RC10', '13760d35-494e-4e50-8428-617d04c4b644')
+  ostype = 'CentOS-7.3'
+
+  # ----- TESTING_MODE
+  context 'When TESTING_MODE flag is on' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), 'true', 'true', default_image, false, ostype)
+      expect(return_image.name).to eql('wmlabs-centos-73-1611-v20180309-1106-snapshot')
+    end
+  end
+
+  context 'When TESTING_MODE flag is off' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), 'true', 'false', default_image, false, ostype)
+      expect(return_image.name).to eql('wmlabs-centos-73-1611-v20180309-1531')
+    end
+  end
+
+  context 'When TESTING_MODE flag is nil' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), 'true', nil, default_image, false, ostype)
+      expect(return_image.name).to eql('wmlabs-centos-73-1611-v20180309-1531')
+    end
+  end
+
+
+  # ----- FAST_IMAGE
+  context 'When FAST_IMAGE flag is false' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), 'false', 'true', default_image, false, ostype)
+      expect(return_image.name).to eql(default_image.name)
+    end
+  end
+
+  context 'When FAST_IMAGE flag is nil' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), nil, 'true', default_image, false, ostype)
+      expect(return_image.name).to eql(default_image.name)
+    end
+  end
+
+
+  # ----- bearmetal
+  context 'When flavor includes baremetal' do
+    it '' do
+      return_image = get_image(images, Image.new('faefeafbaremetalfafefa', 'randomString'), 'true', 'true', default_image, false, ostype)
+      expect(return_image.name).to eql(default_image.name)
+    end
+  end
+
+  # ----- User input
+  context 'When user inputs image ID' do
+    it '' do
+      return_image = get_image(images, Image.new('randomString', 'randomString'), 'true', 'true', default_image, true, ostype)
+      expect(return_image.name).to eql(default_image.name)
+    end
+  end
+end
