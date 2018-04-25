0c3d4f8093f74614093128156cc3107881b92864
Minu Theresa Thomas <mthomas@walmartlabs.com>
2018-04-24 17:27:03 -0700

    Updating the version of solrmonitor version

--- packs/solrcloud.rb
+++ packs/solrcloud.rb
@@ -170,7 +170,7 @@ resource "solrcloud",
     'enable_cinder' => 'true',
     'solr_custom_component_version' => '0.0.3',
     'solr_api_timeout_sec' => '300',
-    'solr_monitor_version' => '1.0.2'
+    'solr_monitor_version' => '1.0.3'
   },
 
   :monitors => {
